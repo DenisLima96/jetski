4613284794f4dc2fb4e71f5aaf6fce3bdad5c55b
DenisLima96 <denisliimati@gmail.com>
2024-07-14 09:27:05 -0300

    primeiro commit

--- jets/admin.py
+++ jets/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from jets.models import jet
+from jets.models import jet, Brand
 
 
 class JetAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year','model_year','value')
     search_fields = ('model',)
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(jet, JetAdmin)
--- jets/models.py
+++ jets/models.py
@@ -1,19 +1,33 @@
 from django.db import models
 
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+        
+
+
 class jet(models.Model):
     #Id do jet
     id = models.AutoField(primary_key=True)
     # Modelo do jetSki
     model = models.CharField(max_length=200)
     # Marca do JetSki
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models. PROTECT, related_name='jet_brand')
     #Ano de Fabricação 
     factory_year = models.IntegerField(blank=True, null=True)
     #Ano do modelo do jet
     model_year = models.IntegerField(blank=True, null=True)
+    #chassi  da embarcação 
+    plate = models.CharField(max_length=10, blank=True, null=True)
     # Valor que é em reais
     value = models.FloatField(blank=True, null=True)
+    #campo de imagem
+    photo =models.ImageField(upload_to='jets/', blank=True, null=True)
+
 
     def __str__(self):
        return self.model
